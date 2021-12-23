9052ec145aac6ff5c9dfd2905924c72cf67ef11c
Tim Hunter <huntert@gmail.com>
2021-12-23 00:11:15 +0000

    update to version 1.1

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Coldest World of FEATURES!"
+    return "Hello Coldest World of FEATURES v1.1!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
